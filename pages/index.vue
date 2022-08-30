36f48554684c34e88003304cdb62a6305e69a458
Candra <candraherdiana78@gmail.com>
2022-08-30 11:30:43 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Html>
+      <Head>
+        <Title>Restaurant</Title>
+        <Meta name="description" content="My name is Candra Herdiana"/>
+        <Link rel="preload" href="https://loram.com" as="script" />
+      </Head>
+    </Html>
+    <Nav />
+    <div class="container">
+      <h1>Welcome to Restaurantly</h1>
+      <a href="/restaurants">Go to restaurants</a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin-top: 5rem;
+}
+</style>
